45e60ef1be3b8973a531e894328cf393b1f5ea07
sahilparmar4 <sahil4parmar@gmail.com>
2025-06-26 00:56:19 +0530

    fix build issues

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,7 +1,8 @@
 import { blogData } from "@/lib/blog";
 import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest } from "next/server";
 
-export async function GET (request: NextApiRequest, response: NextApiResponse){
+export async function GET (request: NextRequest){
     const allBlogs = blogData;
     return Response.json({status: 200, message: "Blogs fetched Successfully", data:allBlogs})
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 "use client"
 import { useEffect } from "react";
 
-export async function getAllBlogs() {
-  const response = await fetch("http://localhost:3000/api/blogs")
-  const data = await response.json()
-  console.log(data.data[0])
-  return data
-}
 
 export default function Home() {
+   async function getAllBlogs() {
+    const response = await fetch("http://localhost:3000/api/blogs")
+    const data = await response.json()
+    console.log(data.data[0])
+    return data
+  }
   useEffect(()=>{
     getAllBlogs()
   }, [])
